10e3c4fe50923904cfe97e4f2227841776135619
Kim Ji Min <moonlight@soomkyung.ac.kr>
2024-02-18 10:51:56 +0900

    b-table striped hover, field, key, label 이용하여 글 정보 그래프로 나타내기

---
+++ src/components/Board.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <b-table striped hover :items="items" :fields="fields"></b-table>
+    </div>
+</template>
+<script>
+import data from '@/components/data/index'
+export default {
+    data() {
+        return{
+            fields: [
+                {
+                    key: 'content_id',
+                    label: '글번호'
+                },
+                {
+                    key: 'title',
+                    label: '글제목'
+                },
+                {
+                    key: 'created_at',
+                    label: '게시 시간'
+                },
+            ],
+            items: data.Content
+        };
+    }
+};
+</script>
